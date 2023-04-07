5a54ecbebb37db4aad99c9cde83712d94b7fc821
Jeffrey Onyediri <jonyediri@gmail.com>
2023-04-07 14:16:40 -0500

    flutter app created

---
+++ lib/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF23264E),
+      body: Center(
+        child: Text('Welcome to the Home Page'),
+      ),
+    );
+  }
+}
